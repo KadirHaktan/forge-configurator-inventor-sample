ab71e9fea8691b6c58b72942035738e04681d95a
KadirHaktan <kadirhaktan.yucesoy@gmail.com>
2023-04-06 13:56:19 +0300

    changing 7

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -72,10 +72,9 @@ namespace WebApplication
                         {
                             long sizeinMB = 1000;
                             long size = sizeinMB * 1024 * 1024;
-                            options.Limits.MaxRequestBodySize = size;
-                            options.ListenAnyIP(5011);
+                            options.Limits.MaxRequestBodySize =  size;
                         });
-                        var port = Environment.GetEnvironmentVariable("PORT") ?? "5011";
+                        var port = Environment.GetEnvironmentVariable("PORT");
                         // If deployed to a service like Heroku, need to listen on port defined in the environment, not the default one
                         if (!string.IsNullOrEmpty(port))
                         {
